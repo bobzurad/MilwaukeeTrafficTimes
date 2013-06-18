7dc74e36bd534ff987674dee679e1aa4bb06b586
bobzurad <bob@zurad.net>
2013-06-17 23:26:38 -0500

    added spinner

--- js/default.js
+++ js/default.js
@@ -1,5 +1,4 @@
 ﻿//TODO: icons
-//TODO: spinner
 //TODO: use minified js
 (function () {
     "use strict";
@@ -11,9 +10,33 @@
     //internal data structures
     var dataRows = [];
     var timestampMessage = null;
+    var $container = null;
     var $tableBody = null;
     var $timestampMessage = null;
     var $cmdRefresh = null;
+    var $spinner = null;
+
+    //spinner options
+    var spinnerOptions = {
+        lines: 13, // The number of lines to draw
+        length: 20, // The length of each line
+        width: 10, // The line thickness
+        radius: 30, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#FFF', // #rgb or #rrggbb
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: 'auto', // Top position relative to parent in px
+        left: 'auto' // Left position relative to parent in px
+    };
+
+    var spinner = new Spinner(spinnerOptions);
 
     //object to store data for each row
     function DataRow(title, time, delay) {
@@ -52,6 +75,11 @@
 
     //function that scraps info from DOT website and sets up internal data structures
     var scrapeDOT = function () {
+        spinner.spin();
+        $spinner.html(spinner.el);
+        $container.hide();
+        $spinner.show();
+
         WinJS.xhr({
             type: "GET",
             url: "http://www.dot.wi.gov/travel/milwaukee/times.htm",
@@ -77,6 +105,10 @@
                 });
                 //generate templates
                 applyTemplates();
+
+                spinner.stop();
+                $spinner.hide();
+                $container.show();
             },
             function error(response) {
 
@@ -98,8 +130,10 @@
             if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
                 // This application has been newly launched. Initialize
                 // your application here.
+                $container = $("#container");
                 $tableBody = $("table#data").find("tbody");
                 $timestampMessage = $("#timestampMessage");
+                $spinner = $("div.spinner");
 
                 $cmdRefresh = $("#RefreshButton");
                 $cmdRefresh.on("click", onCmdRefresh);
